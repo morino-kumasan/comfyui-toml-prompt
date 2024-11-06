e0ad5000b37b1ba37796ac0fed3673172b75bfc4
morino-kumasan <74145127+morino-kumasan@users.noreply.github.com>
2024-11-06 14:17:21 +0900

    プロンプトでLoRAを読み込む

--- __init__.py
+++ __init__.py
@@ -1,5 +1,4 @@
 import re
-import os
 import random
 from functools import reduce
 
@@ -42,7 +41,7 @@ class MultipleLoraLoader:
                 r_model, r_clip = self.loader[i].load_lora(r_model, r_clip, lora_name, strength, strength)
 
         return (r_model, r_clip, '\n'.join([
-            (os.path.basename(kwargs[f"lora_name_{i}"]) if abs(kwargs[f"strength_{i}"]) >= 1e-10 else "")
+            (kwargs[f"lora_name_{i}"] if abs(kwargs[f"strength_{i}"]) >= 1e-10 else "")
             for i in range(0, MAX_LOAD_LORA)
         ]))
 
@@ -50,73 +49,91 @@ class PromptPicker:
     def __init__(self):
         self.encoder = CLIPTextEncode()
         self.concat = ConditioningConcat()
+        self.loader = [LoraLoader() for i in range(0, MAX_LOAD_LORA)]
 
     @classmethod
     def INPUT_TYPES(s):
         return {
             "required": {
+                "model": ("MODEL", {"tooltip": "The diffusion model."}),
                 "clip": ("CLIP", {"tooltip": "The CLIP model."}),
                 "key_name_list": ("STRING", {"multiline": True, "tooltip": "Select Key Name"}),
                 "text": ("STRING", {"multiline": True, "dynamicPrompts": True, "defaultInput": True, "tooltip": "Prompt Separated by Key Name Comment (#[keyname])"}),
             }
         }
 
-    RETURN_TYPES = ("CONDITIONING", )
-    OUTPUT_TOOLTIPS = ("A Conditioning containing a text by key_name.", )
+    RETURN_TYPES = ("MODEL", "CLIP", "CONDITIONING", "STRING")
+    OUTPUT_TOOLTIPS = ("The diffusion model.", "The CLIP model.", "A Conditioning containing a text by key_name.", "Loaded LoRA name list")
     FUNCTION = "load_prompt"
 
     CATEGORY = "conditioning"
     DESCRIPTION = "LoRA prompt load."
 
-    def load_prompt(self, clip, text, key_name_list):
+    def load_prompt(self, model, clip, text, key_name_list):
+        sep = '.'
         key = ""
         before_end = 0
-        r_text = {}
+        prompt_dict = {}
 
         # Find key_name
         for m in re.finditer(r'^#\[(.+)\]$', text, flags=re.MULTILINE):
             beg, end = (m.start(), m.end())
             if beg != before_end and key != "":
-                r_text[key] = text[before_end:beg]
+                prompt_dict[key] = text[before_end:beg]
             key = m.group(1)
             before_end = end
 
         if before_end != len(text):
-            r_text[key] = text[before_end:]
+            prompt_dict[key] = text[before_end:]
+
+        r_cond = None
+        r_model = model
+        r_clip = clip
+        r_loras = ""
+        keys = prompt_dict.keys()
+        lora_i = 0
 
         # Pick key_name and Encode Prompt
-        r = None
-        keys = r_text.keys()
-        sep = '.'
         for key in key_name_list.splitlines():
             key = key.strip()
             if key == "":
                 continue
 
+            # Collect Prompts
             key_parts = key.split(sep)
             if key_parts[-1] == '?':
                 prefix = sep.join(key_parts[:-1] + [''])
                 random_keys = [key for key in keys if key if keys and key.startswith(prefix) and not key.endswith("*")]
                 available_keys = sorted([sep.join(key_parts[:l] + ["*"]) for l in range(0, len(key_parts))]) + [random.choice(random_keys)]
-                prompts = [','.join([r_text[key] for key in available_keys if key in keys])]
+                prompts = [','.join([prompt_dict[key] for key in available_keys if key in keys])]
             else:
                 available_keys = sorted([sep.join(key_parts[:l] + ["*"]) for l in range(0, len(key_parts))]) + [key]
-                prompts = [','.join([r_text[key] for key in available_keys if key in keys])]
+                prompts = [','.join([prompt_dict[key] for key in available_keys if key in keys])]
 
+            # Encode Prompts
             for prompt in prompts:
                 prompt = prompt.strip()
                 if prompt == "":
                     continue
+
+                loras = re.findall(r'<lora:([^:]+):([0-9.]+)>', prompt)
+                for lora_name, strength in loras:
+                    r_model, r_clip = self.loader[lora_i].load_lora(r_model, r_clip, lora_name, float(strength), float(strength))
+                    lora_i += 1
+                    r_loras += lora_name + '\n'
+                    print(f"lora loaded: {lora_name}: {strength}")
+                prompt = re.sub(r'<lora:([^:]+):([0-9.]+)>', '', prompt)
+
                 cond = self.encoder.encode(clip, prompt)[0]
-                if r is None:
-                    r = cond
+                if r_cond is None:
+                    r_cond = cond
                 else:
-                    r = self.concat.concat(cond, r)[0]
+                    r_cond = self.concat.concat(cond, r_cond)[0]
 
-        if r is None:
-            r = self.encoder.encode(clip, "")[0]
+        if r_cond is None:
+            r_cond = self.encoder.encode(clip, "")[0]
 
-        return (r, )
+        return (r_model, r_clip, r_cond, r_loras)
 
 class PromptHolder:
     def __init__(self):
@@ -140,16 +157,67 @@ class PromptHolder:
     def load_prompt(self, text):
         return (text, )
 
+class MultilineStringConcat:
+    def __init__(self):
+        pass
+
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "text_from": ("STRING", {"defaultInput": True, "multiline": True, "tooltip": "Text from."}),
+                "text_to": ("STRING", {"defaultInput": True, "multiline": True, "tooltip": "Text to."}),
+            }
+        }
+
+    RETURN_TYPES = ("STRING", )
+    OUTPUT_TOOLTIPS = ("A text.", )
+    FUNCTION = "concat"
+
+    CATEGORY = "utils"
+    DESCRIPTION = "Concat string."
+
+    def concat(self, text_from, text_to):
+        return (text_to + '\n' + text_from, )
+
+class StringSub:
+    def __init__(self):
+        pass
+
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "text": ("STRING", {"defaultInput": True, "multiline": True, "tooltip": "Text."}),
+                "pattern": ("STRING", {"tooltip": "Matching regex pattern."}),
+                "to": ("STRING", {"tooltip": "Matching text to."}),
+            }
+        }
+
+    RETURN_TYPES = ("STRING", )
+    OUTPUT_TOOLTIPS = ("A text.", )
+    FUNCTION = "sub"
+
+    CATEGORY = "utils"
+    DESCRIPTION = "Concat string."
+
+    def sub(self, text, pattern, to):
+        return (re.sub(pattern, to, text), )
+
 NODE_CLASS_MAPPINGS = {
     "MultipleLoraLoader": MultipleLoraLoader,
     "PromptPicker": PromptPicker,
     "PromptHolder": PromptHolder,
+    "MultilineStringConcat": MultilineStringConcat,
+    "StringSub": StringSub,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "MultipleLoraLoader": "Load Multiple Loras With Output Lora Name",
-    "PromptPicker": "Load Prompt by Key",
-    "PromptHolder": "Hold Prompt",
+    "MultipleLoraLoader": "MultipleLoraLoader",
+    "PromptPicker": "PromptPicker",
+    "PromptHolder": "PromptHolder",
+    "MultilineStringConcat": "MultilineStringConcat",
+    "StringSub": "StringSub",
 }
 
 __all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS"]
