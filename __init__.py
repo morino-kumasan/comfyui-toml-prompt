26d1121486f47ca6c8e271a9be6a2d135c04c412
morino-kumasan <74145127+morino-kumasan@users.noreply.github.com>
2024-11-10 04:27:36 +0900

    LoRAの読み込みをTomlPromptEncoderに集約

--- __init__.py
+++ __init__.py
@@ -2,96 +2,21 @@ import os
 import re
 import hashlib
 import random
-import time
 import toml
 from functools import reduce
 
 from folder_paths import get_filename_list, get_user_directory
 from nodes import LoraLoader, CLIPTextEncode, ConditioningConcat
 
-MAX_LOAD_LORA = 10
+def remove_comment_out(s):
+    return re.sub(r"((//|#).+$|/\*.*?\*/)", "", s).strip()
 
-def escape_lora_filename(filename):
-    return re.sub(r"\.", r"\\.", re.sub(r"\\", r"\\\\", filename))
-
-class MultipleLoraLoader:
-    def __init__(self):
-        self.loader = [LoraLoader() for i in range(0, MAX_LOAD_LORA)]
-
-    @classmethod
-    def INPUT_TYPES(s):
-        lora_file_list = get_filename_list("loras")
-        return {
-            "required": { k: v for k, v in [
-                ("model", ("MODEL", {"tooltip": "The diffusion model."})),
-                ("clip", ("CLIP", {"tooltip": "The CLIP model."})),
-            ] + reduce(lambda x, y: x + y, [[
-                (f"lora_name_{i}", (lora_file_list, {"tooltip": "LoRA file name."})),
-                (f"strength_{i}", ("FLOAT", {"default": 0.0, "min": -100.0, "max": 100.0, "step": 0.01, "tooltip": "Modify strength."})),
-            ] for i in range(0, MAX_LOAD_LORA)]) }
-        }
-
-    RETURN_TYPES = ("MODEL", "CLIP", "STRING")
-    OUTPUT_TOOLTIPS = ("The diffusion model.", "The CLIP model.", "Loaded LoRA name list")
-    FUNCTION = "load_lora"
-
-    CATEGORY = "loaders"
-    DESCRIPTION = "LoRAs load."
-
-    def load_lora(self, model, clip, **kwargs):
-        r_model = model
-        r_clip = clip
-        for i in range(0, MAX_LOAD_LORA):
-            lora_name = kwargs[f"lora_name_{i}"]
-            strength = kwargs[f"strength_{i}"]
-            if abs(strength) >= 1e-10:
-                r_model, r_clip = self.loader[i].load_lora(r_model, r_clip, lora_name, strength, strength)
-
-        return (r_model, r_clip, '\n'.join([
-            escape_lora_filename(kwargs[f"lora_name_{i}"])
-            for i in range(0, MAX_LOAD_LORA) if abs(kwargs[f"strength_{i}"]) >= 1e-10
-        ]))
-
-def split_key(key_str, sep):
-    l = [(0, 0)] + [(m.start() + 1, m.end()) for m in re.finditer(f"[^\\\\]\\{sep}", key_str)] + [(len(key_str), len(key_str))]
-    keys = [re.sub(f"\\\\\\{sep}", sep, key_str[l[i][1]:l[i + 1][0]]) for i in range(0, len(l) - 1)]
-    return keys
-
-def collect_prompts(prompt_dict, key_str):
-    r = []
-    d = prompt_dict
-    keys = split_key(key_str, ".")
-    for i, key in enumerate(keys):
-        if key == "?" or key == "??":
-            assert(key == "?" or i == (len(keys) - 1))
-            recur = key == "??"
-            rand_keys = [k for k in d.keys() if not k.startswith("_")]
-            if len(rand_keys) == 0:
-                return r
-            key = random.choice(rand_keys)
-        else:
-            recur = False
-
-        if key not in d:
-            print(f"Key Not Found: {key}")
-            return r
-
-        d = d[key]
-        if "_t" in d:
-            if '$' in d["_t"] and "_v" not in d:
-                print(f"_v Not Set: {d}")
-            t = re.sub(r"\${([a-zA-Z0-9_-]+)}", lambda m: random.choice(d["_v"][m.group(1)]), d["_t"])
-            r += [t]
-
-        if recur:
-            r += collect_prompts(d, "??")
-    return r
-
-class PromptPicker:
-    def __init__(self):
-        self.encoder = CLIPTextEncode()
-        self.concat = ConditioningConcat()
-        self.loader = [LoraLoader() for i in range(0, MAX_LOAD_LORA)]
+class TomlPromptEncoder:
+    RETURN_TYPES = ("MODEL", "CLIP", "CONDITIONING", "STRING", "STRING", "INT")
+    OUTPUT_TOOLTIPS = ("The diffusion model.", "The CLIP model.", "A Conditioning containing a text by key_name.", "Loaded LoRA name list", "A prompt", "Random seed")
+    FUNCTION = "load_prompt"
+    CATEGORY = "conditioning"
+    DESCRIPTION = "LoRA prompt load."
 
     @classmethod
     def INPUT_TYPES(s):
@@ -101,78 +26,149 @@ class PromptPicker:
                 "clip": ("CLIP", {"tooltip": "The CLIP model."}),
                 "key_name_list": ("STRING", {"multiline": True, "dynamicPrompts": True, "tooltip": "Select Key Name"}),
                 "seed": ("INT", {"default": 0, "min": 0, "max": 0xffffffffffffffff, "tooltip": "Random seed."}),
-                "text": ("STRING", {"multiline": True, "dynamicPrompts": True, "defaultInput": True, "tooltip": "Prompt Separated by Key Name Comment (#[keyname])"}),
+                "text": ("STRING", {"multiline": True, "dynamicPrompts": True, "defaultInput": True, "tooltip": "TOML format prompt."}),
+                "lora_info": ("STRING", {"multiline": True, "dynamicPrompts": True, "defaultInput": True, "tooltip": "TOML format lora prompt."}),
             }
         }
 
-    @classmethod
-    def IS_CHANGED(s, *args, **kwargs):
-        return time.time()
+    def __init__(self):
+        self.encoder = CLIPTextEncode()
+        self.concat = ConditioningConcat()
+        self.loader = {}
+        self.loras = []
+        self.prompt = []
+        self.loaded_keys = []
 
-    RETURN_TYPES = ("MODEL", "CLIP", "CONDITIONING", "STRING", "STRING", "INT")
-    OUTPUT_TOOLTIPS = ("The diffusion model.", "The CLIP model.", "A Conditioning containing a text by key_name.", "Loaded LoRA name list", "A prompt", "Random seed")
-    FUNCTION = "load_prompt"
+    def collect_prompts(self, prompt_dict, keys, prefix):
+        r = []
+        d = prompt_dict
+        for i, key in enumerate(keys):
+            if key == "?" or key == "??":
+                assert(key == "?" or i == (len(keys) - 1))
+                recur = key == "??"
+                rand_keys = [k for k in d.keys() if not k.startswith("_")]
+                if len(rand_keys) == 0:
+                    return r
+                key = random.choice(rand_keys)
+            else:
+                recur = False
 
-    CATEGORY = "conditioning"
-    DESCRIPTION = "LoRA prompt load."
+            if key not in d:
+                print(f"Key Not Found: {key}")
+                return r
 
-    def load_lora_from_prompt(self, prompt, model, clip, lora_i):
+            prefix += [key]
+            prefix_str = '.'.join(prefix)
+            d = d[key]
+
+            if prefix_str not in self.loaded_keys and "_t" in d:
+                self.loaded_keys += [prefix_str]
+                if '$' in d["_t"] and "_v" not in d:
+                    print(f"_v Not Set: {d}")
+                t = re.sub(r"\${([a-zA-Z0-9_-]+)}", lambda m: random.choice(d["_v"][m.group(1)]), d["_t"])
+                r += [t]
+
+            if recur:
+                r += self.collect_prompts(d, ["??"], prefix)
+        return r
+
+    def load_lora_from_prompt(self, prompt, lora_dict, model, clip):
         r_model = model
         r_clip = clip
-        loras = []
         for lora_name, strength in re.findall(r'<lora:([^:]+):([0-9.]+)>', prompt):
-            i = len(loras) + lora_i
-            r_model, r_clip = self.loader[i].load_lora(r_model, r_clip, lora_name, float(strength), float(strength))
-            loras += [escape_lora_filename(lora_name)]
-            print(f"Lora Loaded[{i}]: {lora_name}: {strength}")
-        prompt = re.sub(r'<lora:([^:]+):([0-9.]+)>', '', prompt)
-        return (r_model, r_clip, loras)
+            if lora_name not in self.loader:
+                self.loader[lora_name] = LoraLoader()
+                r_model, r_clip = self.loader[lora_name].load_lora(r_model, r_clip, lora_name, float(strength), float(strength))
+                print(f"Lora Loaded: {lora_name}: {strength}")
+            self.loras += ["<lora:{}:{}>".format(lora_name, strength)]
+        prompt = re.sub(r'<lora:([^:]+):([0-9.]+)>', lambda m: ','.join(self.collect_prompts(lora_dict, [m.group(1).replace("\\", "\\\\")], [])), prompt)
+        return (r_model, r_clip, prompt)
 
-    def encode_prompt(self, prompt, model, clip, cond, loras, lora_i):
+    def encode_prompt(self, prompt, lora_dict, model, clip, cond):
         r_model = model
         r_clip = clip
         r_cond = cond
         prompt = prompt.strip()
         if prompt == "":
-            return (r_model, r_clip, r_cond, loras, lora_i)
+            return (r_model, r_clip, r_cond)
 
-        r_model, r_clip, loaded_loras = self.load_lora_from_prompt(prompt, r_model, r_clip, lora_i)
-        lora_i += len(loaded_loras)
-        loras += loaded_loras
+        r_model, r_clip, prompt = self.load_lora_from_prompt(prompt, lora_dict, r_model, r_clip)
+        self.prompt += [prompt]
 
         cond = self.encoder.encode(r_clip, prompt)[0]
         if r_cond is None:
             r_cond = cond
         else:
             r_cond = self.concat.concat(cond, r_cond)[0]
-        return (r_model, r_clip, r_cond, loras, lora_i)
+        return (r_model, r_clip, r_cond)
 
-    def load_prompt(self, model, clip, seed, text, key_name_list):
+    def load_prompt(self, model, clip, seed, text, lora_info, key_name_list):
         random.seed(seed)
+        self.loader = {}
+        self.loras = []
+        self.prompt = []
+        self.loaded_keys = []
+
         r_cond = None
         r_model = model
         r_clip = clip
-        r_loras = []
-        r_prompt = []
-        lora_i = 0
         prompt_dict = toml.loads(text)
+        lora_dict = toml.loads(lora_info)
         for key_str in key_name_list.splitlines():
-            key_str = re.sub(r"((//|#).+$|/\*.*?\*/)", "", key_str).strip()
-            if key_str == "" or key_str.startswith("#") or key_str.startswith("//"):
+            key_str = remove_comment_out(key_str)
+            if key_str == "":
                 continue
-            keys = [k.strip() for k in split_key(key_str, "&")]
-            prompt = ','.join([','.join(collect_prompts(prompt_dict, key)) for key in keys])
-            r_model, r_clip, r_cond, r_loras, lora_i = self.encode_prompt(prompt, r_model, r_clip, r_cond, r_loras, lora_i)
-            r_prompt += [prompt]
+
+            prompts = []
+            for key in [k.strip() for k in key_str.split("&")]:
+                m = re.match(r'^<lora:([^:]+):([0-9.]+)>$', key)
+                if m:
+                    r_model, r_clip, prompt = self.load_lora_from_prompt(key, lora_dict, r_model, r_clip)
+                    prompts += [prompt]
+                else:
+                    prompts += [','.join(self.collect_prompts(prompt_dict, key.split("."), []))]
+            prompt = ','.join(prompts)
+
+            r_model, r_clip, r_cond = self.encode_prompt(prompt, lora_dict, r_model, r_clip, r_cond)
 
         if r_cond is None:
             r_cond = self.encoder.encode(clip, "")[0]
 
-        return (r_model, r_clip, r_cond, '\n'.join(r_loras), '\nBREAK\n'.join(r_prompt), seed)
+        return (r_model, r_clip, r_cond, '\n'.join(self.loras), '\nBREAK\n'.join([p for p in self.prompt if p]), seed)
 
-class PromptLoader:
+class MultipleLoraTagLoader:
+    RETURN_TYPES = ("STRING", )
+    OUTPUT_TOOLTIPS = ("LoRA tag list separated by line break", )
+    FUNCTION = "create_tags"
+    CATEGORY = "utils"
+    DESCRIPTION = "Create LoRA tags."
+    MAX_TAG_LORA = 10
+
+    @classmethod
+    def INPUT_TYPES(s):
+        lora_file_list = get_filename_list("loras")
+        return {
+            "required": { k: v for k, v in reduce(lambda x, y: x + y, [[
+                (f"lora_name_{i}", (lora_file_list, {"tooltip": "LoRA file name."})),
+                (f"strength_{i}", ("FLOAT", {"default": 0.0, "min": -100.0, "max": 100.0, "step": 0.01, "tooltip": "Modify strength."})),
+            ] for i in range(0, MultipleLoraTagLoader.MAX_TAG_LORA)]) }
+        }
+
     def __init__(self):
         pass
+
+    def create_tags(self, **kwargs):
+        return ('\n'.join([
+            "<lora:{}:{:2f}>".format(kwargs[f"lora_name_{i}"], kwargs[f"strength_{i}"])
+            for i in range(0, MultipleLoraTagLoader.MAX_TAG_LORA) if abs(kwargs[f"strength_{i}"]) >= 1e-10
+        ]), )
+
+class PromptLoader:
+    RETURN_TYPES = ("STRING", )
+    OUTPUT_TOOLTIPS = ("A Prompt.", )
+    FUNCTION = "load_prompt"
+    CATEGORY = "utils"
+    DESCRIPTION = "Prompt loader."
 
     @classmethod
     def INPUT_TYPES(s):
@@ -190,12 +186,8 @@ class PromptLoader:
             m.update(f.read())
         return m.digest().hex()
 
-    RETURN_TYPES = ("STRING", )
-    OUTPUT_TOOLTIPS = ("A Prompt.", )
-    FUNCTION = "load_prompt"
-
-    CATEGORY = "utils"
-    DESCRIPTION = "Prompt loader."
+    def __init__(self):
+        pass
 
     def load_prompt(self, file):
         path = os.path.expanduser(file)
@@ -204,8 +196,11 @@ class PromptLoader:
         return (text, )
 
 class StringConcat:
-    def __init__(self):
-        pass
+    RETURN_TYPES = ("STRING", )
+    OUTPUT_TOOLTIPS = ("A text.", )
+    FUNCTION = "concat"
+    CATEGORY = "utils"
+    DESCRIPTION = "Concat string."
 
     @classmethod
     def INPUT_TYPES(s):
@@ -217,17 +212,19 @@ class StringConcat:
             }
         }
 
-    RETURN_TYPES = ("STRING", )
-    OUTPUT_TOOLTIPS = ("A text.", )
-    FUNCTION = "concat"
-
-    CATEGORY = "utils"
-    DESCRIPTION = "Concat string."
+    def __init__(self):
+        pass
 
     def concat(self, text_from, text_to, sep):
         return (text_to + sep + text_from, )
 
 class StringSub:
+    RETURN_TYPES = ("STRING", )
+    OUTPUT_TOOLTIPS = ("A text.", )
+    FUNCTION = "sub"
+    CATEGORY = "utils"
+    DESCRIPTION = "Concat string."
+
     def __init__(self):
         pass
 
@@ -241,19 +238,16 @@ class StringSub:
             }
         }
 
-    RETURN_TYPES = ("STRING", )
-    OUTPUT_TOOLTIPS = ("A text.", )
-    FUNCTION = "sub"
-
-    CATEGORY = "utils"
-    DESCRIPTION = "Concat string."
-
     def sub(self, text, pattern, to):
         return (re.sub(pattern, to, text, flags=re.MULTILINE), )
 
 class StringViewer:
-    def __init__(self):
-        pass
+    RETURN_TYPES = ("STRING", )
+    OUTPUT_TOOLTIPS = ("A text.", )
+    FUNCTION = "view_str"
+    OUTPUT_NODE = True
+    CATEGORY = "utils"
+    DESCRIPTION = "String Viewer."
 
     @classmethod
     def INPUT_TYPES(s):
@@ -263,20 +257,15 @@ class StringViewer:
             }
         }
 
-    RETURN_TYPES = ("STRING", )
-    OUTPUT_TOOLTIPS = ("A text.", )
-    FUNCTION = "view_str"
-    OUTPUT_NODE = True
-
-    CATEGORY = "utils"
-    DESCRIPTION = "String Viewer."
+    def __init__(self):
+        pass
 
     def view_str(self, text):
         return {"ui": { "text": [text] }, "result": (text,)}
 
 NODE_CLASS_MAPPINGS = {
-    "MultipleLoraLoader": MultipleLoraLoader,
-    "PromptPicker": PromptPicker,
+    "TomlPromptEncoder": TomlPromptEncoder,
+    "MultipleLoraTagLoader": MultipleLoraTagLoader,
     "PromptLoader": PromptLoader,
     "StringConcat": StringConcat,
     "StringSub": StringSub,
@@ -284,8 +273,8 @@ NODE_CLASS_MAPPINGS = {
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "MultipleLoraLoader": "MultipleLoraLoader",
-    "PromptPicker": "PromptPicker",
+    "TomlPromptEncoder": "TomlPromptEncoder",
+    "MultipleLoraTagLoader": "MultipleLoraTagLoader",
     "PromptLoader": "PromptLoader",
     "StringConcat": "StringConcat",
     "StringSub": "StringSub",
