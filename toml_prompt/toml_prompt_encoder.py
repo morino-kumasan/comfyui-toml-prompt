38b03bc3dc9f89f1d236a87fbe970ae5f38d122f
morino-kumasan <74145127+morino-kumasan@users.noreply.github.com>
2024-11-11 20:31:51 +0900

    整理

--- toml_prompt/toml_prompt_encoder.py
+++ toml_prompt/toml_prompt_encoder.py
@@ -1,6 +1,7 @@
 import re
 import random
 import toml
+import functools
 
 from nodes import LoraLoader, CLIPTextEncode, ConditioningConcat
 
@@ -9,6 +10,91 @@ def remove_comment_out(s):
 
 def select_dynamic_prompt(s):
     return re.sub(r"{([^}]+)}", lambda m: random.choice(m.group(1).split('|')).strip(), s)
+
+def expand_prompt_var(d):
+    def random_var(m):
+        if "_v" not in d:
+            print(f"_v Not Set: {d}")
+            return ""
+        return random.choice(d["_v"][m.group(1)])
+    return re.sub(r"\${([a-zA-Z0-9_-]+)}", random_var, d["_t"])
+
+def get_keys_all(d):
+    return [k for k in d.keys() if not k.startswith("_")]
+
+def get_keys_all_recursive(d, prefix=[]):
+    r = []
+    if get_keys_all(d) == 0:
+        return [".".join(prefix)]
+    for k, v in d.items():
+        if k.startswith("_"):
+            continue
+        if "_t" in v:
+            r += ['.'.join(prefix + [k])]
+        r += get_keys_all_recursive(v, prefix + [k])
+    return r
+
+def get_keys_random(d):
+    rand_keys = get_keys_all(d)
+    return random.choice(rand_keys)
+
+def get_keys_random_recursive(d):
+    rand_keys = get_keys_all_recursive(d)
+    return random.choice(rand_keys)
+
+def build_search_keys(keys, prefix=[]):
+    assert len(keys) > 0
+    if isinstance(keys, str):
+        keys = [(key.split("+")) for key in keys.split(".")]
+    if len(keys) == 1:
+        return [".".join(prefix + [key]) for key in keys[0]]
+    return functools.reduce(lambda x, y: x + y, [[".".join(prefix + [key])] + build_search_keys(keys[1:], prefix + [key]) for key in keys[0]])
+
+def collect_prompt(prompt_dict, keys, exclude_keys=None, init_prefix=None):
+    if isinstance(keys, str):
+        keys = build_search_keys(keys)
+
+    if exclude_keys is None:
+        exclude_keys = []
+
+    r = []
+    for key in keys:
+        d = prompt_dict
+        key_parts = key.split(".")
+        prefix = init_prefix or []
+        while len(key_parts) > 0:
+            key = key_parts.pop(0)
+            if key == "?":
+                key = get_keys_random(d)
+            elif key == "??":
+                assert len(key_parts) == 0
+                keys = get_keys_random_recursive(d)
+                r += collect_prompt(d, keys, exclude_keys, prefix[:])
+                break
+            elif key == "*":
+                keys = [".".join([key] + key_parts) for key in get_keys_all(d)]
+                r += collect_prompt(d, keys, exclude_keys, prefix[:])
+                break
+            elif key == "**":
+                assert len(key_parts) == 0
+                keys = get_keys_all_recursive(d)
+                r += collect_prompt(d, keys, exclude_keys, prefix[:])
+                break
+
+            if key not in d:
+                print(f"Key Not Found: {'.'.join(prefix + [key])}")
+                break
+            d = d[key]
+            prefix += [key]
+        else:
+            prefix_str = ".".join(prefix)
+            if "_t" in d:
+                if prefix_str not in exclude_keys:
+                    r += [select_dynamic_prompt(remove_comment_out(expand_prompt_var(d)))]
+                    exclude_keys += [prefix_str]
+            else:
+                print(f"Key Not Found: {prefix_str}")
+    return r
 
 class TomlPromptEncoder:
     RETURN_TYPES = ("MODEL", "CLIP", "CONDITIONING", "STRING", "STRING", "INT")
@@ -38,41 +124,6 @@ class TomlPromptEncoder:
         self.prompt = []
         self.loaded_keys = []
 
-    def collect_prompts(self, prompt_dict, keys, prefix):
-        r = []
-        d = prompt_dict
-        for i, key in enumerate(keys):
-            if key == "?" or key == "??":
-                assert(key == "?" or i == (len(keys) - 1))
-                recur = key == "??"
-                rand_keys = [k for k in d.keys() if not k.startswith("_")]
-                if len(rand_keys) == 0:
-                    return r
-                key = random.choice(rand_keys)
-            else:
-                recur = False
-                rand_keys = []
-
-            if key not in d:
-                print(f"Key Not Found: [{'.'.join(prefix)}.{key}]: {rand_keys}")
-                return r
-
-            prefix += [key]
-            prefix_str = '.'.join(prefix)
-            d = d[key]
-
-            if prefix_str not in self.loaded_keys and "_t" in d:
-                self.loaded_keys += [prefix_str]
-                if '$' in d["_t"] and "_v" not in d:
-                    print(f"_v Not Set: {d}")
-                t = re.sub(r"\${([a-zA-Z0-9_-]+)}", lambda m: random.choice(d["_v"][m.group(1)]), d["_t"])
-                t = select_dynamic_prompt(remove_comment_out(t))
-                r += [t]
-
-            if recur:
-                r += self.collect_prompts(d, ["??"], prefix)
-        return r
-
     def load_lora_from_prompt(self, prompt, lora_dict, model, clip):
         r_model = model
         r_clip = clip
@@ -82,7 +133,7 @@ class TomlPromptEncoder:
                 r_model, r_clip = self.loader[lora_name].load_lora(r_model, r_clip, lora_name, float(strength), float(strength))
                 print(f"Lora Loaded: {lora_name}: {strength}")
             self.loras += ["<lora:{}:{}>".format(lora_name, strength)]
-        prompt = re.sub(r'<lora:([^:]+):([0-9.]+)>', lambda m: ','.join(self.collect_prompts(lora_dict, [m.group(1).replace("\\", "\\\\")], [])), prompt)
+        prompt = re.sub(r'<lora:([^:]+):([0-9.]+)>', lambda m: ','.join(collect_prompt(lora_dict, [m.group(1).replace("\\", "\\\\")])), prompt)
         return (r_model, r_clip, prompt)
 
     def encode_prompt(self, prompt, lora_dict, model, clip, cond):
@@ -127,7 +178,7 @@ class TomlPromptEncoder:
                     r_model, r_clip, prompt = self.load_lora_from_prompt(key, lora_dict, r_model, r_clip)
                     prompts += [prompt]
                 else:
-                    prompts += [','.join(self.collect_prompts(prompt_dict, key.split("."), []))]
+                    prompts += [','.join(collect_prompt(prompt_dict, build_search_keys(key), exclude_keys=self.loaded_keys))]
             prompt = ','.join(prompts)
 
             r_model, r_clip, r_cond = self.encode_prompt(prompt, lora_dict, r_model, r_clip, r_cond)
