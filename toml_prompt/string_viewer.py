e5a97895d7733ca49abb5f430148c1ed5c3413ee
morino-kumasan <74145127+morino-kumasan@users.noreply.github.com>
2024-11-17 05:18:16 +0900

    生成結果をextra_pnginfoに書き込む

--- toml_prompt/string_viewer.py
+++ toml_prompt/string_viewer.py
@@ -11,11 +11,18 @@ class StringViewer:
         return {
             "required": {
                 "text": ("STRING", {"forceInput": True, "multiline": True, "tooltip": "file name."}),
-            }
+            },
+            "hidden": {
+                "unique_id": "UNIQUE_ID",
+                "extra_pnginfo": "EXTRA_PNGINFO"
+            },
         }
 
     def __init__(self):
         pass
 
-    def view_str(self, text):
-        return {"ui": { "text": [text] }, "result": (text,)}
+    def view_str(self, text, unique_id=None, extra_pnginfo=None):
+        if extra_pnginfo is not None:
+            node = [node for node in extra_pnginfo["workflow"]["nodes"] if node["id"] == int(unique_id)][0]
+            node["widgets_values"][1] = text
+        return {"ui": { "text": [text] }}
